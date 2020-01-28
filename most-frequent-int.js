8f0f8678179adfb67f6a7ff850a0a47819504c40
Michael <mihendricks1@gmail.com>
2020-01-28 17:46:36 +0200

    Most frequent int javascript solution

---
+++ most-frequent-int.js
@@ -0,0 +1,45 @@
+/* 
+  Given an array of integers, find the most frequent integer.
+  Write a method that takes an array of integers and returns an integer
+*/
+
+const mostFrequentInt = (arr) => {
+  if (!(arr instanceof Array)) return;
+
+  // I guess, I'll create a dictionary to keep try of each integers then increment the value the number of times it exists
+  const intCountDict = {};
+
+
+  console.log("For array:", arr);
+  arr.forEach((item) => {
+    if (intCountDict.hasOwnProperty(item)) {
+      intCountDict[item]++;
+    } else {
+      intCountDict[item] = 1;
+    }
+  });
+
+  let maxInt = 0;
+  let maxItem = {};
+  for (let item in intCountDict) {
+    if (intCountDict[item] > maxInt) {
+      maxItem = item;
+      maxInt = intCountDict[item];
+    }
+  }
+
+  
+  console.log("Dictionary:", intCountDict);
+  console.log("Max prop: ", maxItem);
+  return maxItem;
+}
+
+const arr1 = [1, 2, 3, 3];        // => 3
+const arr2 = [1, 2, 3];           // => 1 or 2 or 3
+const arr3 = [3, 1, 4, 57, 4];    // => 4
+const arr4 = {};    // => 4
+
+console.log(mostFrequentInt(arr4));
+console.log(mostFrequentInt(arr1));
+console.log(mostFrequentInt(arr2));
+console.log(mostFrequentInt(arr3));
